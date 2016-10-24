fc86a902e3d2f0ce37c5e0ce1357b4f2a833e1b3
Harjito <titounnes@users.noreply.github.com>
2016-10-24 23:26:51 +0700

    Create menu.php

---
+++ application/config/menu.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['folder1'] = [
+    'kategori1' => [
+        'contrller11' => 'parameter1/parameter2',
+        'controller12' => '',
+        'controller13' => 'parameter1',
+    ],
+    'kategori2' => [
+        'controller21' => 'parameter1',
+        'subkategiri2' => [
+          'controller121' => 'parameter1',
+          'controller122' => '',
+    ],
+    'controller3' => '',
+];
