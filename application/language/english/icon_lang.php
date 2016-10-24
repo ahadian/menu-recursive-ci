5b6e9f519e126165d9859d32d10f3ee866836a50
Harjito <titounnes@users.noreply.github.com>
2016-10-24 23:34:58 +0700

    Create icon_lang.php

---
+++ application/language/english/icon_lang.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['controller11_icon']			='edit';
+$lang['controller12_icon']			='user';
+$lang['controller13_icon']			='list';
+
