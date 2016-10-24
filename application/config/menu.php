e4684e18847a261357dba30d15c0f60dd41bf1e9
Harjito <titounnes@users.noreply.github.com>
2016-10-24 23:30:50 +0700

    Update menu.php

--- application/config/menu.php
+++ application/config/menu.php
@@ -14,3 +14,18 @@ $config['folder1'] = [
     ],
     'controller3' => '',
 ];
+    
+$config['folder2'] = [
+    'kategor3' => [
+        'contrller31' => 'parameter1/parameter2',
+        'controller32' => '',
+        'controller33' => 'parameter1',
+    ],
+    'kategori4' => [
+        'controller41' => 'parameter1',
+        'subkategir3' => [
+          'controller411' => 'parameter1',
+          'controller412' => '',
+    ],
+    'controller5' => '',
+];
